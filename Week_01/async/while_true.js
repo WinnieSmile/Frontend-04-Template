396b3a1316f09c9c1f3ec4a63727a572c1a42c84
wangting <1581404884@qq.com>
2020-09-01 23:52:07 +0800

    异步编程

---
+++ Week_01/async/while_true.js
@@ -0,0 +1,27 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-01 23:49:47
+ * @LastEditTime: 2020-09-01 23:50:01
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \前端训练营\作业提交\Frontend-04-Template\Week_01\async\while_true.js
+ */
+
+ 
+
+/*  while(true){} 里面没有break，这种写法在同步代码中不会出现，在异步代码中很常见。
+    业务需求：做一个表盘、操作系统的事件循环。 while(true)使用场景
+*/
+async function* counter(){
+    let i = 0;
+    while(true){   
+        await sleep(1000);
+        yield i++;
+    }
+}
+// 异步函数声明
+(async function(){
+    for await(let v of counter()){
+        console.log(v);
+    }
+})()
